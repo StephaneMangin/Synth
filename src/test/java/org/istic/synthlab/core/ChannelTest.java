565586cffbd1e329c8707e5f22bc1256e508dd9c
paola93 <npaolita@yahoo.fr>
2016-02-02 18:21:16 +0100

    test of channel

--- src/test/java/org/istic/synthlab/core/ChannelTest.java
+++ src/test/java/org/istic/synthlab/core/ChannelTest.java
@@ -1,29 +1,45 @@
 package org.istic.synthlab.core;
 
-import static org.junit.Assert.*;
+
+import com.jsyn.ports.UnitInputPort;
+import com.jsyn.ports.UnitOutputPort;
+import javafx.util.Pair;
+import junit.framework.TestCase;
 
 /**
- * Created by stephane on 02/02/16.
+ * Created by paola on 02/02/16.
  */
-public class ChannelTest {
+public class ChannelTest extends TestCase {
+
+    private Channel channel;
+    private IInput input;
+    private IOutput outPut;
 
     @org.junit.Before
     public void setUp() throws Exception {
-
+        channel = new Channel();
     }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-
-    }
-
     @org.junit.Test
     public void testConnect() throws Exception {
+        input = new InputAdapter(new UnitInputPort("port"));
+        outPut = new OutputAdapter(new UnitOutputPort("port"));
 
+        channel.connect(input, outPut);
+        Pair<IInput, IOutput> pair =  new Pair<>(input, outPut);
+        assertFalse(channel.getConnections().isEmpty());
+        assertTrue(channel.getConnections().size() == 1);
+        assertTrue(channel.getConnections().contains(pair));
     }
 
     @org.junit.Test
     public void testDisconnect() throws Exception {
+        input = new InputAdapter(new UnitInputPort("port1"));
+        outPut = new OutputAdapter(new UnitOutputPort("port2"));
 
+        channel.connect(input,outPut);
+        channel.disconnect(input, outPut);
+        Pair<IInput, IOutput> pair =  new Pair<>(input, outPut);
+        assertTrue(channel.getConnections().isEmpty());
+        assertFalse(channel.getConnections().contains(pair));
     }
 }
