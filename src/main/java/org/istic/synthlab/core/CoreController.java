d0924985b50b855014126a1969094f977547858f
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-02-02 16:15:12 +0100

    Fix the warnings in core.fxml Add a controller (CoreController) for core.fxml

---
+++ src/main/java/org/istic/synthlab/core/CoreController.java
@@ -0,0 +1,17 @@
+package org.istic.synthlab.core;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CoreController implements Initializable {
+    @FXML
+    public ListView listView;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
