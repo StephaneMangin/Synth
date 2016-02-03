abaf2136f85547d1959d3f7bc6b211345d26d292
augustinBardou <augustin.bardou@gmail.com>
2016-02-03 14:08:56 +0100

    Merge branch '#GLOB' of /home/augustin/Documents/COURS_M2/ProjectSynthLab with conflicts.

--- src/main/java/org/istic/synthlab/core/CoreController.java
+++ src/main/java/org/istic/synthlab/core/CoreController.java
@@ -1,30 +1,68 @@
 package org.istic.synthlab.core;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
+
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CoreController implements Initializable {
+
+    private ObservableList data;
+
+    private Node nodeComponentsList;
+
+    @FXML
+    public ListView listView;
+    @FXML
+    public BorderPane mainFrame;
+    @FXML
+    public GridPane gridPane;
     @FXML
     public ListView<Label> listView;
     @FXML
     public GridPane gridPane;
 
     public void initialize(URL location, ResourceBundle resources) {
+        data = FXCollections.observableArrayList();
+        listView.setItems(addComponents2List());
+        System.out.println(gridPane);
+    }
+    public ObservableList addComponents2List(){
+        data = FXCollections.observableArrayList();
+        for(int i=0;i<5;i++){
+            try {
+                nodeComponentsList = FXMLLoader.load(getClass().getResource("/ListViewComponents.fxml"));
+                nodeComponentsList.setId("paneComponents"+i);
+                data.add(nodeComponentsList);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
         // Populate the ListView
         Label testLabel = new Label("VCOA");
         listView.setItems(FXCollections.observableArrayList(testLabel));
 
+    public void initGrid(){
+        gridPane.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> {
+            for(Node node : gridPane.getChildren()){
+                if(node instanceof Pane){
+                    if(node.getBoundsInParent().contains(event.getSceneX(), event.getSceneY())){
+                        System.out.println( "Node: " + node + " at " + GridPane.getRowIndex( node) + "/" + GridPane.getColumnIndex( node));
+                    }
+                }
+            }
+        });
         // Label d&d events
         testLabel.setOnDragDetected(event -> {
             Dragboard db = testLabel.startDragAndDrop(TransferMode.COPY);
