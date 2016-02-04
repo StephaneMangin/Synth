713ed315625dae546e4100f84878efbe3af519fb
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-02-04 10:11:42 +0100

    Add @author

--- src/main/java/org/istic/synthlab/core/Main.java
+++ src/main/java/org/istic/synthlab/core/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Thibaut Rousseau <thibaut.rousseau@outlook.com>
+ */
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
