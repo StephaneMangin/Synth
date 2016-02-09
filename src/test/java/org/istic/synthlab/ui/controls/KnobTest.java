cae5c864a0390f58787e75e04697e725dc92cbbd
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-02-09 13:33:57 +0100

    Improve KnobTest

--- src/test/java/org/istic/synthlab/ui/controls/KnobTest.java
+++ src/test/java/org/istic/synthlab/ui/controls/KnobTest.java
@@ -12,35 +12,37 @@ public class KnobTest {
 
     private Knob knob;
     private Random random;
+
     /**
-     * Arbitrary delta for floating point comparisons
+     * Arbitrary epsilon for floating point comparisons
      * Taken from here: https://stackoverflow.com/a/8385364/1544176 and tested empirically, 1e-16 is too small
      */
-    public static final double DELTA = 1e-15;
+    public static final double EPSILON = 1e-15;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         knob = new Knob();
-        random = new Random();
+        random = new Random(123456789L); // We use a fixed seed so the tests are repeatable
     }
 
     @After
-    public void tearDown() throws Exception {
-
+    public void tearDown() {
+        knob = null;
+        random = null;
     }
 
     @Test
     public void testMinAndMaxAngle() {
-        assertEquals(0, knob.angleToValue(Knob.MIN_ANGLE), DELTA);
-        assertEquals(1, knob.angleToValue(Knob.MAX_ANGLE), DELTA);
+        assertEquals(0, knob.angleToValue(Knob.MIN_ANGLE), EPSILON);
+        assertEquals(1, knob.angleToValue(Knob.MAX_ANGLE), EPSILON);
     }
 
     @Test
     public void testAngleToValueIsOppositeOfValueToAngle() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1_000_000; i++) {
             final double expected = random.nextDouble();
             final double actual = knob.angleToValue(knob.valueToAngle(expected));
-            assertEquals(expected, actual, DELTA);
+            assertEquals(expected, actual, EPSILON);
         }
     }
 }
