8ccd2e4fb7e5bab715a32b9e574e54859f1bb874
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-02-10 00:14:24 +0100

    Merge Knob2 with Potentiometer

--- src/main/java/org/istic/synthlab/ui/controls/Potentiometer.java
+++ src/main/java/org/istic/synthlab/ui/controls/Potentiometer.java
@@ -2,41 +2,29 @@ package org.istic.synthlab.ui.controls;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Bounds;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 
-import javax.xml.bind.annotation.XmlAttribute;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 /**
  * Created by blacknight on 09/02/16.
  */
 public class Potentiometer extends Pane {
-
     @FXML
     public Button rotatorDial;
-
     @FXML
     public Button rotatorHandle;
 
     private static final double HEIGHT = 36;
     private static final double WIDTH = 36;
-
-    private double rotation;
+    private static final double MIN = -140;
+    private static final double MAX = 140;
 
     private final DoubleProperty value = new SimpleDoubleProperty(0);
 
@@ -52,28 +40,27 @@ public class Potentiometer extends Pane {
         value.set(newValue);
     }
 
-    // JavaFX has a weird behavior, see docs/knob.png to check how the angles look like on the circle
-    // The MIN_ANGLE is bigger than the MAX_ANGLE because the upper part of the circle is negative
-    public static final double MIN_ANGLE = 120;
-    public static final double MAX_ANGLE = 420;
-
+    /**
+     * This constructor loads the FXML associated to the potentiometer, binds the event handlers and initializes the view
+     */
     public Potentiometer() {
-        super();
-        setPrefHeight(HEIGHT);
-        setPrefWidth(WIDTH);
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/controls/potentiometer.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
         try {
+            final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/controls/potentiometer.fxml"));
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
             fxmlLoader.load();
-        } catch (IOException exception) {
+        } catch (final IOException exception) {
             throw new RuntimeException(exception);
         }
-        rotatorDial.setOnMouseEntered(new MaximiseEventHandler());
-        rotatorHandle.setOnMouseEntered(new MaximiseEventHandler());
-        rotatorDial.setOnMouseDragged(new ClickKnobEventHandler());
+
+        setPrefHeight(HEIGHT);
+        setPrefWidth(WIDTH);
+        setRotate(-90); // Because we want 0 to be at the bottom to simplify some calculations
+        //rotatorDial.setOnMouseEntered(new MaximiseEventHandler());
+        //rotatorHandle.setOnMouseEntered(new MaximiseEventHandler());
+        rotatorDial.setOnMouseDragged(new DragKnobEventHandler());
         rotatorDial.setOnScroll(new ScrollKnobEventHandler());
+        rotateHandle(MIN);
     }
 
     private class MaximiseEventHandler implements EventHandler<MouseEvent> {
@@ -103,20 +90,14 @@ public class Potentiometer extends Pane {
     private class ScrollKnobEventHandler implements EventHandler<ScrollEvent> {
         @Override
         public void handle(final ScrollEvent event) {
-            if ((int) Math.signum(event.getDeltaY()) > 0) {
-                rotation += 5;
-            } else {
-                rotation -= 5;
-            }
-            setRotation(rotation);
+            rotateHandle(rotatorHandle.getRotate() + 10 * Math.signum(event.getDeltaY()));
             event.consume();
         }
     }
 
-    private class ClickKnobEventHandler implements EventHandler<MouseEvent> {
+    private class DragKnobEventHandler implements EventHandler<MouseEvent> {
         @Override
         public void handle(final MouseEvent event) {
-            // Where did we click?
             double x = event.getX(), y = event.getY();
 
             // Out of bounds values are possible
@@ -139,8 +120,6 @@ public class Potentiometer extends Pane {
             x -= WIDTH/2;
             y -= HEIGHT/2;
 
-            System.out.println("(" + x + " ; " + y + ")");
-
             // Calculate the angle between (x ; y) and the center of the circle
             //
             //        |
@@ -153,17 +132,18 @@ public class Potentiometer extends Pane {
             //        y
             //
             // tan(x, y) = y/x (ie. opposite/adjacent)
-            setRotation(Math.toDegrees(Math.atan2(y, x)));
+            rotateHandle(Math.toDegrees(Math.atan2(y, x)));
             event.consume();
         }
     }
 
-    private void setRotation(double value) {
-        if (rotation <= MIN_ANGLE) {
-            rotation = MIN_ANGLE;
-        } else if (rotation >= MAX_ANGLE) {
-            rotation = MAX_ANGLE;
+    /**
+     * Rotate the handle by taking in consideration the MIN and MAX values
+     * @param degrees The angle to which the handle will be rotated, relative to the start position, not the current one
+     */
+    private void rotateHandle(final double degrees) {
+        if (degrees >= MIN && degrees <= MAX) {
+            rotatorHandle.setRotate(degrees);
         }
-        rotatorHandle.setRotate(rotation);
     }
 }
