c2bf679035c52eee4080a6b078b2835f666ea0ea
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-02-22 11:59:30 +0100

    VCFA template

---
+++ src/main/java/org/istic/synthlab/components/vcfa/Controller.java
@@ -0,0 +1,19 @@
+package org.istic.synthlab.components.vcfa;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.istic.synthlab.components.AbstractController;
+import org.istic.synthlab.ui.controls.Potentiometer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller extends AbstractController implements Initializable {
+    @FXML
+    public Potentiometer cutoffFrequency;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
